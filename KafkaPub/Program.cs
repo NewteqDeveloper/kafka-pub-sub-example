526fc29407a9bedeaaa6915b69ad684a1410ef8f
Newteq <newt.fourie@entelect.co.za>
2019-09-20 14:24:40 +0200

    add the code

---
+++ KafkaPub/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaPub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            Action<DeliveryReport<Null, string>> handler = r => Console.WriteLine(
+                !r.Error.IsError
+                    ? $"Message delivered :) to {r.TopicPartitionOffsetError}"
+                    : $"Message delivery failed :( awwww REASON? -- {r.Error.Reason}");
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                for (var i = 0; i < 100; i++)
+                {
+                    producer.Produce("sample-topic", new Message<Null, string>() { Value = $"Publisher message - {i}" }, handler);
+                    Thread.Sleep(1000);
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
---
+++ KafkaSub/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, string>
+            {
+                {"group.id", "sample-consumers"},
+                {"bootstrap.servers", "localhost:9092"},
+                {"enable.auto.commit", "false"}
+            };
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("sample-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch(Exception)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
